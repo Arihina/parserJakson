5e28f5f559971fb097b53079eea34f6c695b7cc0
Alena Kozlova <alena.kozlova2004@mail.ru>
2024-06-09 20:00:27 +0300

    add crypto parser

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,16 +1,21 @@
 package org.example;
 
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class Main {
     public static void main(String[] args) {
+        fiatParse();
+        cryptoParse();
+    }
+
+    public static void fiatParse() {
         try {
             URL url = new URL("https://www.cbr-xml-daily.ru/daily_json.js");
             HttpURLConnection connection = (HttpURLConnection) url.openConnection();
@@ -31,10 +36,42 @@ public class Main {
             String valutes = nodes.get("Valute").toString();
 
             nodes = mapper.readTree(valutes);
-            for (var node : nodes)
-            {
+            for (var node : nodes) {
                 System.out.print(node.get("CharCode") + " ");
                 System.out.println(node.get("Value"));
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void cryptoParse() {
+        try {
+            URL url = new URL("https://api.binance.com/api/v3/ticker/price");
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+
+            BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            StringBuilder response = new StringBuilder();
+            String inputLine;
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+            in.close();
+
+            String jsonResponse = response.toString();
+            ObjectMapper mapper = new ObjectMapper();
+
+            JsonNode json = mapper.readTree(jsonResponse);
+
+            var pairs = json.findValues("symbol");
+            var prices = json.findValues("price");
+
+            for (int i = 0; i < pairs.size(); i++) {
+                if (pairs.get(i).toString().contains("USDT")) {
+                    System.out.println(pairs.get(i) + " : " + prices.get(i));
+                }
             }
 
         } catch (Exception e) {
